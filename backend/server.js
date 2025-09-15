7212e6cb769744a0b446101a64c7adf530101f6b
priya duhan <priya>
2025-09-15 15:52:27 +0530

    changes in server.js

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,35 @@ import todoRoutes from "./routes/todoRoutes.js";
 import userRoutes from "./routes/userDataRoutes.js";
 import cookieParser from "cookie-parser";
 
+import cors from "cors";
+
+const allowedOrigins = [
+  "http://localhost:5173", // local dev frontend
+  "https://todo-mern-gray.vercel.app/login", // deployed frontend
+];
+
 dotenv.config({
   path: "./env",
 });
 const PORT = process.env.PORT || 9000;
 
 const app = express();
+app.use(
+  cors({
+    origin: allowedOrigins,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+    credentials: true,
+  })
+);
+app.options(
+  "*",
+  cors({
+    origin: allowedOrigins,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
